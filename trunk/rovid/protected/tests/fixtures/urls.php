effdd48d629f00d8a9df440cd006d2b812225412
imehesz <imehesz@5b76c444-987a-11de-8efd-97b2a7529d09>
2010-06-03 17:22:32 +0000

    added the URL model and started to setup the tests

---
+++ trunk/rovid/protected/tests/fixtures/urls.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+	/*
+	'sample1'=>array(
+		'created' => '',
+		'shortened' => '',
+		'url' => '',
+	),
+	'sample2'=>array(
+		'created' => '',
+		'shortened' => '',
+		'url' => '',
+	),
+	*/
+);
