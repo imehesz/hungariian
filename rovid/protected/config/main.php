3422dc6bbd46bf05a3ea65ed59b1c4e2f940148c
imehesz <imehesz@5b76c444-987a-11de-8efd-97b2a7529d09>
2010-06-03 17:22:32 +0000

    added the URL model and started to setup the tests

--- rovid/protected/config/main.php
+++ rovid/protected/config/main.php
@@ -36,7 +36,7 @@ return array(
 		),
 		*/
 		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/rovidke.db',
 		),
 		// uncomment the following to use a MySQL database
 		/*
---
+++ rovid/protected/tests/unit/UrlTest.php
@@ -0,0 +1,13 @@
+<?php
+
+class UrlTest extends CDbTestCase
+{
+	public $fixtures=array(
+		'urls'=>'Url',
+	);
+
+	public function testCreate()
+	{
+
+	}
+}
